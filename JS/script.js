dc22d3360ec6c377f4643a53d597c1984708745e
Bellatrix-Salungweni1 <bellatrixsalungweni106@student.unsrat.ac.id>
2023-03-30 09:30:06 +0700

    Dept & non-dept

--- JS/script.js
+++ JS/script.js
@@ -17,6 +17,113 @@ toggleSwitch.addEventListener('click', function(e) {
   document.body.className = checked ? 'dark' : '' 
 })
 
+//department//
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  sliderPerGroup: 3,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 0,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".slide-container1", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  sliderPerGroup: 3,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+
+  navigation: {
+    nextEl: ".swiper-navBtn1",
+    prevEl: ".swiper-navBtn1",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1000: {
+      slidesPerView: 1,
+    },
+  },
+});
+
+var swiper = new Swiper(".slide-container2", {
+  slidesPerView: 3,
+  spaceBetween: 0,
+  sliderPerGroup: 3,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+
+  navigation: {
+    nextEl: ".swiper-navBtn2",
+    prevEl: ".swiper-navBtn2",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+  o.addEventListener("click", () => {
+    selected.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+  });
+});
+//departent//
 
 function animateValue(obj, start, end, duration) {
   let startTimestamp = null;
